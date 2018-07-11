fd2596d2d5ef673ef17dc85e32b69cf63c5618d5
Mike Eastes <michael.eastes@gmail.com>
2018-07-10 20:19:16 -0500

    Adds CSS module support.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.module.scss';
 import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1 className={styles.appTitle}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={styles.appIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
