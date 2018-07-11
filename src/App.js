2c7fdf34bbf16b8d80b48bb645f5b7027b82d3ac
Mike Eastes <michael.eastes@gmail.com>
2018-07-10 21:04:12 -0500

    Removes boilerplate CRA code.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import React, { Component } from 'react';
 import styles from './App.module.scss';
 import ContentWrapper from './components/ContentWrapper';
 import Hero from './components/Hero';
-import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.app}>
-        <Hero />
-        <ContentWrapper>Content Here</ContentWrapper>
-        <header className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1 className={styles.appTitle}>Welcome to React</h1>
+        <header>
+          <Hero />
         </header>
-        <p className={styles.appIntro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContentWrapper>Content Here</ContentWrapper>
       </div>
     );
   }
