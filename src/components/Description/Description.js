3533d82291215bdc8ac61890f121148ba90ab11c
Mike Eastes <michael.eastes@gmail.com>
2018-07-12 19:45:21 -0500

    Adds typography styling to description.

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Typography } from '../../../node_modules/@material-ui/core';
 import styles from './Description.module.scss';
 
 export default () => (
-  <p className={styles.description}>
+  <Typography component="p" className={styles.description}>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ligula odio, rhoncus quis urna
     dapibus, vehicula interdum urna. Nullam condimentum magna id sapien feugiat, non consectetur
     lacus fringilla.
-  </p>
+  </Typography>
 );
